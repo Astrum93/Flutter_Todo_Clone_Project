7b1acf0e85bb8002d9df42148d3eaea8247b8598
Astrum93 <jungmin2670@gmail.com>
2023-11-23 02:01:03 +0900

    231123 Todo

--- lib/screen/main/tab/tab_item.dart
+++ lib/screen/main/tab/tab_item.dart
@@ -4,24 +4,27 @@ import 'package:fast_app_base/screen/main/tab/home/f_home.dart';
 import 'package:flutter/material.dart';
 
 enum TabItem {
-  home(Icons.home, '홈', HomeFragment()),
-  favorite(Icons.star, '즐겨찾기', FavoriteFragment(isShowBackButton: false));
+  todo(Icons.event_note_outlined, 'Todo', HomeFragment()),
+  search(Icons.search, 'Search', FavoriteFragment(isShowBackButton: false));
 
   final IconData activeIcon;
   final IconData inActiveIcon;
   final String tabName;
   final Widget firstPage;
 
-  const TabItem(this.activeIcon, this.tabName, this.firstPage, {IconData? inActiveIcon})
+  const TabItem(this.activeIcon, this.tabName, this.firstPage,
+      {IconData? inActiveIcon})
       : inActiveIcon = inActiveIcon ?? activeIcon;
 
-  BottomNavigationBarItem toNavigationBarItem(BuildContext context, {required bool isActivated}) {
+  BottomNavigationBarItem toNavigationBarItem(BuildContext context,
+      {required bool isActivated}) {
     return BottomNavigationBarItem(
         icon: Icon(
           key: ValueKey(tabName),
           isActivated ? activeIcon : inActiveIcon,
-          color:
-              isActivated ? context.appColors.iconButton : context.appColors.iconButtonInactivate,
+          color: isActivated
+              ? context.appColors.iconButton
+              : context.appColors.iconButtonInactivate,
         ),
         label: tabName);
   }
