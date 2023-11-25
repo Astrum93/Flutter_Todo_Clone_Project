02a2556f217756403fc057a6b70a9da6c1c5e325
Astrum93 <jungmin2670@gmail.com>
2023-11-25 20:54:29 +0900

    231123_Todo Clone Project_WriteTodo 생성

---
+++ lib/screen/main/write/d_write_todo.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WriteTodoDialog extends StatefulWidget {
+  const WriteTodoDialog({super.key});
+
+  @override
+  State<WriteTodoDialog> createState() => _WriteTodoDialogState();
+}
+
+class _WriteTodoDialogState extends State<WriteTodoDialog> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
